fef77410a805e05e24539dae409a61ba63741dbd
DepthDeluxe <cheinzmann3@gmail.com>
2014-06-29 08:24:29 -0700

    added 404: not found page

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
-var ENODENT = fs.ENODENT;
 var express = require('express');
 var router = express.Router();
 
+// allow client to get favicon
 router.get('/favicon.ico', function(req, res) {
      console.log('getting favicon');
      res.status(200).end('./public/favicon.ico');
 });
 
+// index redirection page
 router.get('/', function(req, res) {
      res.redirect('/index');
 });
@@ -15,7 +16,7 @@ router.get('/', function(req, res) {
 // sends the homepage
 router.get('/*', function(req, res) {
      // attempt to load the content
-     console.log(req.url);
+     console.log(req.ip + ' requesting ' + req.url);
      var content;
      try {
           content = fs.readFileSync( './src/pages' + req.url + '.htm');
@@ -27,10 +28,10 @@ router.get('/*', function(req, res) {
                res.status(200).send(html).end();
           });
      } catch(err) {
-          if (err !== ENODENT) throw err;
+          if (err.code !== "ENOENT") throw err;
 
           console.log('Error: not found');
-          res.status(404).end();
+          res.status(404).redirect('/404');
      }
 });
 
